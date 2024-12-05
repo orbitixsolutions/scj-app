030edadfb1ac1704d964c592d362e685a695be4d
miguel angel <162249501+orbitixsolutions@users.noreply.github.com>
2024-12-04 21:07:14 -0500

    CHORE: setup sonner toaster 📢

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import localFont from 'next/font/local'
 import './globals.css'
+import { Toaster } from '@/components/ui/sonner'
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
@@ -33,6 +34,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${mermaid.variable} antialiased`}
       >
+        <Toaster position='top-center' />
         {children}
       </body>
     </html>
