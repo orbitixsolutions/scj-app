d338e1c3c84b7554502ef4a4ae9d8f650de8fef4
miguel angel <162249501+orbitixsolutions@users.noreply.github.com>
2024-12-05 12:34:41 -0500

    CHORE: add user zod schema 🗒️

--- schemas/index.ts
+++ schemas/index.ts
@@ -8,3 +8,27 @@ export const LoginSchema = z.object({
     message: 'La contraseña debe tener al menos 8 caracteres',
   }),
 })
+
+export const UserSchema = z.object({
+  name: z.string().min(2, {
+    message: 'El nombre es inválido',
+  }),
+  lastName: z.string().min(2, {
+    message: 'El apellido es inválido',
+  }),
+  email: z.string().email({
+    message: 'El email es inválido',
+  }),
+  password: z.string().min(8, {
+    message: 'La contraseña debe tener al menos 8 caracteres',
+  }),
+  documentIdentity: z.string().min(2, {
+    message: 'La identidad es inválida',
+  }),
+  role: z.enum(['DIRECTIVE', 'ADMIN', 'TEACHER', 'STUDENT', 'DEVELOPER'], {
+    message: 'El rol es inválido',
+  }),
+  description: z.string().min(2, {
+    message: 'La descripción es inválida',
+  }),
+})
