5d32206963f73b840829ae9abecd54a6bd830f82
miguel angel <162249501+orbitixsolutions@users.noreply.github.com>
2024-12-05 12:37:26 -0500

    CHORE: add roles list 🗒️

--- constants/index.ts
+++ constants/index.ts
@@ -1,1 +1,24 @@
 export const PAGE_NAME = 'Los pinos'
+
+export const SELECT_ROLES = [
+  {
+    value: 'DIRECTIVE',
+    label: 'Directivo',
+  },
+  {
+    value: 'ADMIN',
+    label: 'Administrador',
+  },
+  {
+    value: 'TEACHER',
+    label: 'Profesor',
+  },
+  {
+    value: 'STUDENT',
+    label: 'Estudiante',
+  },
+  {
+    value: 'DEVELOPER',
+    label: 'Desarrollador',
+  },
+]
