ef585c2f7816eaf2dc9697d55fa2c1ae45edcf0d
miguel angel <162249501+orbitixsolutions@users.noreply.github.com>
2024-12-04 19:28:58 -0500

    FEAT: add get users actions 🔨

---
+++ lib/users.ts
@@ -0,0 +1,29 @@
+import db from '@/lib/db'
+
+export async function getUserByEmail(email: string) {
+  try {
+    const USER = await db.user.findUnique({
+      where: {
+        email,
+      },
+    })
+
+    return USER
+  } catch {
+    return null
+  }
+}
+
+export async function getUserById(id: string) {
+  try {
+    const USER = await db.user.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    return USER
+  } catch {
+    return null
+  }
+}
