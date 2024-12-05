2f9958447ad05df81bde3858f9ae49e9f9d29d54
miguel angel <162249501+orbitixsolutions@users.noreply.github.com>
2024-12-05 12:40:01 -0500

    FEAT: add dialog form component 🎨

---
+++ components/dialog-form/dialog-form.tsx
@@ -0,0 +1,79 @@
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog'
+import { DialogFormProps } from '@/components/dialog-form/dialog-form.type'
+import { Button } from '@/components/ui/button'
+import { Pencil } from 'lucide-react'
+
+export function DialogForm(props: DialogFormProps) {
+  const {
+    title,
+    description,
+    disabled,
+    isEditing,
+    isOpen,
+    setIsOpen,
+    children,
+    formId,
+  } = props
+
+  return (
+    <Dialog
+      open={isOpen}
+      onOpenChange={setIsOpen}
+    >
+      <DialogTrigger asChild>
+        {isEditing ? (
+          <Button
+            disabled={disabled}
+            size='icon'
+            variant='outline'
+          >
+            <Pencil />
+          </Button>
+        ) : (
+          <Button
+            disabled={disabled}
+            variant='outline'
+          >
+            Crear
+          </Button>
+        )}
+      </DialogTrigger>
+      <DialogContent className='w-[720px] max-w-full'>
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+          {description && <DialogDescription>{description}</DialogDescription>}
+        </DialogHeader>
+
+        {children}
+
+        <DialogFooter className='grid grid-cols-2 gap-4'>
+          <DialogClose asChild>
+            <Button
+              type='button'
+              variant='outline'
+            >
+              Cancelar
+            </Button>
+          </DialogClose>
+
+          <Button
+            form={formId}
+            disabled={disabled}
+            type='submit'
+          >
+            {isEditing ? 'Guardar' : 'Crear'}
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  )
+}
---
+++ components/dialog-form/dialog-form.type.ts
@@ -0,0 +1,10 @@
+export type DialogFormProps = {
+  title: string
+  description?: string
+  disabled?: boolean
+  isEditing?: boolean
+  children: React.ReactNode
+  formId: string
+  isOpen?: boolean
+  setIsOpen?: React.Dispatch<React.SetStateAction<boolean>>
+}
