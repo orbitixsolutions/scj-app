90201a971813a5ab3225bd3648f36250d22f9f2c
miguel angel <162249501+orbitixsolutions@users.noreply.github.com>
2024-12-05 12:36:53 -0500

    FEAT: auth hook session 🔒

---
+++ hooks/use-session.tsx
@@ -0,0 +1,11 @@
+import { useSession } from 'next-auth/react'
+
+export const useCurrentRole = () => {
+  const session = useSession()
+  return session.data?.user?.role
+}
+
+export const useCurrentUser = () => {
+  const session = useSession()
+  return session.data?.user
+}
