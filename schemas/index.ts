24d0701975fb27a389b4e6520439e3b760a72765
miguel angel <162249501+orbitixsolutions@users.noreply.github.com>
2024-12-04 21:08:41 -0500

    CHORE: add passowrd user field 🔒

--- schemas/index.ts
+++ schemas/index.ts
@@ -4,4 +4,7 @@ export const LoginSchema = z.object({
   email: z.string().email({
     message: 'El email es inválido',
   }),
+  password: z.string().min(8, {
+    message: 'La contraseña debe tener al menos 8 caracteres',
+  }),
 })
