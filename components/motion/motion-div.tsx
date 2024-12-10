5c096c9d9be5630776e7aebef38c48496afd1311
miguel angel <162249501+orbitixsolutions@users.noreply.github.com>
2024-12-10 03:38:22 -0500

    FEAT: add motion html elements ⭐

---
+++ components/motion/motion-div.tsx
@@ -0,0 +1,8 @@
+'use client'
+
+import { motion } from 'framer-motion'
+
+export const MotionDiv = motion.div
+export const MotionUl = motion.ul
+export const MotionLi = motion.li
+export const MotionSpan = motion.span
