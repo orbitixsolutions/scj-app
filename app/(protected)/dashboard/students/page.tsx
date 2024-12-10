f8f73f01162b7676ff11511d9e798cd5df2f6ca3
miguel angel <162249501+orbitixsolutions@users.noreply.github.com>
2024-12-10 03:39:19 -0500

    FEAT: setup students page ✅

---
+++ app/(protected)/dashboard/students/page.tsx
@@ -0,0 +1,32 @@
+import { ContentLayout } from '@/components/content-layout'
+import { CreateStudentForm } from '@/app/(protected)/dashboard/students/_components/create-student-form'
+import { StudentList } from '@/app/(protected)/dashboard/students/_components/student-list'
+import { getStudents } from '@/app/(protected)/dashboard/students/_services/fetch'
+import { PageProps } from '@/app/(protected)/dashboard/students/_types'
+import { StudentFilter } from '@/app/(protected)/dashboard/students/_components/student-filter'
+import { Students } from '@prisma/client'
+import { StudentBatchDelete } from '@/app/(protected)/dashboard/students/_components/student-batch-delete'
+import { StudentBatchProvider } from '@/providers/student-batch-provider'
+
+export default async function StudentsPage(props: PageProps) {
+  const { searchParams: PARAMS } = props
+  const STUDENTS = (await getStudents(PARAMS)) as Array<Students>
+
+  return (
+    <ContentLayout title='Alumnos'>
+      <header className='flex items-center justify-between'>
+        <h2 className='font-bold text-xl'>Alumnos</h2>
+        <CreateStudentForm />
+      </header>
+      <StudentBatchProvider>
+        <section className='space-y-4'>
+          <StudentFilter />
+
+          <StudentList data={STUDENTS} />
+        </section>
+
+        <StudentBatchDelete />
+      </StudentBatchProvider>
+    </ContentLayout>
+  )
+}
