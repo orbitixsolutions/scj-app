63cfb68866f59c8a3fc922e3b6c12f85828740e3
miguel angel <162249501+orbitixsolutions@users.noreply.github.com>
2024-12-10 03:40:54 -0500

    FEAT: add student batch checkbox component 🎨

---
+++ app/(protected)/dashboard/students/_components/student-batch-checkbox/student-batch-checkbox.tsx
@@ -0,0 +1,34 @@
+'use client'
+
+import { Checkbox } from '@/components/ui/checkbox'
+import { FormControl, FormField, FormItem } from '@/components/ui/form'
+import { useStudentBatch } from '@/providers/student-batch-provider'
+import { StudentBatchCheckboxProps } from '@/app/(protected)/dashboard/students/_components/student-batch-checkbox/student-batch-checkbox.type'
+
+export function StudentBatchCheckbox(props: StudentBatchCheckboxProps) {
+  const { id } = props
+
+  const { form, isDeleting } = useStudentBatch()
+  if (!isDeleting) return null
+
+  return (
+    <FormField
+      control={form.control}
+      name='items'
+      render={({ field }) => (
+        <FormItem className='absolute top-4 left-4'>
+          <FormControl>
+            <Checkbox
+              checked={field.value?.includes(id)}
+              onCheckedChange={(checked) => {
+                return checked
+                  ? field.onChange([...field.value, id])
+                  : field.onChange(field.value.filter((item) => item !== id))
+              }}
+            />
+          </FormControl>
+        </FormItem>
+      )}
+    />
+  )
+}
