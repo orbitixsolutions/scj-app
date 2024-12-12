b96c35e82533f6ad062fea6d707a1fe037aa1072
miguel angel <162249501+orbitixsolutions@users.noreply.github.com>
2024-12-12 16:18:34 -0500

    CHORE: add react query provider

--- providers/index.tsx
+++ providers/index.tsx
@@ -1,8 +1,11 @@
 'use client'
 
 import * as React from 'react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
 import { SessionProvider } from 'next-auth/react'
+
+const queryClient = new QueryClient()
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
@@ -12,7 +15,11 @@ export function Providers({ children }: { children: React.ReactNode }) {
       enableSystem
       disableTransitionOnChange
     >
-      <SessionProvider>{children}</SessionProvider>
+      <SessionProvider>
+        <QueryClientProvider client={queryClient}>
+          {children}
+        </QueryClientProvider>
+      </SessionProvider>
     </NextThemesProvider>
   )
 }
