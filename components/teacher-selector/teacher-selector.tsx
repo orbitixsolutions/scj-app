2bb305941e6285647980a7e0dea770787441c9dc
miguel angel <162249501+orbitixsolutions@users.noreply.github.com>
2024-12-12 16:26:53 -0500

    FEAT: teacher selector component 🎨

---
+++ components/teacher-selector/teacher-selector.tsx
@@ -0,0 +1,101 @@
+'use client'
+
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover'
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandItem,
+  CommandList,
+} from '@/components/ui/command'
+import { useFetch } from '@/hooks/use-fetch'
+import { Button } from '@/components/ui/button'
+import { User } from '@prisma/client'
+import { Check, ChevronsUpDown } from 'lucide-react'
+import { TeacherSelectorProps } from '@/components/teacher-selector/teacher-selector.type'
+import { cn } from '@/lib/utils'
+import { useMemo } from 'react'
+import Link from 'next/link'
+
+export function TeacherSelector(props: TeacherSelectorProps) {
+  const { value, disabled, onChange } = props
+
+  const TEACHER_END_POINT = '/api/v0/dashboard/teachers'
+  const { data } = useFetch<User[]>(TEACHER_END_POINT)
+
+  const TEACHERS = useMemo(
+    () =>
+      data.map((teacher) => ({
+        label: `${teacher.name} ${teacher.lastName}`,
+        value: teacher.id,
+      })),
+    [data]
+  )
+
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button
+          disabled={disabled}
+          variant='outline'
+          role='combobox'
+          className={cn(
+            'w-full justify-between',
+            !value && 'text-muted-foreground'
+          )}
+        >
+          {value
+            ? TEACHERS.find((teacher) => teacher.value === value)?.label
+            : 'Selecciona un profesor...'}
+          <ChevronsUpDown className='opacity-50' />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className='w-80 p-0'>
+        <Command className='w-full'>
+          <CommandList>
+            <CommandEmpty>
+              <article className='text-muted-foreground'>
+                <p>No se encontraron profesores.</p>
+                <p>
+                  Asigne un{' '}
+                  <span>
+                    <Link
+                      className='underline '
+                      href={'/dashboard/create-user'}
+                    >
+                      profesor
+                    </Link>
+                  </span>{' '}
+                  a este taller.
+                </p>
+              </article>
+            </CommandEmpty>
+            <CommandGroup>
+              {TEACHERS.map((teacher) => (
+                <CommandItem
+                  key={teacher.value}
+                  value={teacher.value}
+                  onSelect={(currentValue) => {
+                    onChange(currentValue === value ? '' : currentValue)
+                  }}
+                >
+                  {teacher.label}
+                  <Check
+                    className={cn(
+                      'ml-auto',
+                      value === teacher.value ? 'opacity-100' : 'opacity-0'
+                    )}
+                  />
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  )
+}
---
+++ components/teacher-selector/teacher-selector.type.ts
@@ -0,0 +1,5 @@
+export type TeacherSelectorProps = {
+  value: string
+  onChange: (...value: unknown[]) => void
+  disabled?: boolean
+}
