fdc382b0de9792768c5669bb47c24acbef4fdaf0
miguel angel <162249501+orbitixsolutions@users.noreply.github.com>
2024-12-18 16:13:33 -0500

    STYLE: add path to tailwind content 🎨

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,7 @@ const config: Config = {
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './constants/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     fontFamily: {
