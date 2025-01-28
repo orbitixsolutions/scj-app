d1bb8a2a515eebd96b4a680d4f0b0e6f2f3dc7ff
miguel angel <162249501+orbitixsolutions@users.noreply.github.com>
2025-01-28 07:25:33 -0500

    CHORE: add date input skeleton component 🎨

--- components/skeletons/skeletons.tsx
+++ components/skeletons/skeletons.tsx
@@ -4,6 +4,10 @@ export function InputSkeleton() {
   return <Skeleton className='h-9 w-full' />
 }
 
+export function DateInputSkeleton() {
+  return <Skeleton className='h-8 w-full' />
+}
+
 export function ButtonIconSkeleton() {
   return <Skeleton className='h-9 w-9' />
 }
